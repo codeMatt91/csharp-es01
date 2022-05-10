61e81608c37c34de1a8e64c1dcf15c50c381fbfd
Matteo <Matteo@MATT>
2022-05-10 17:26:45 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Chiedo all'utente due numeri
+
+            Console.WriteLine("inserisci il primo numero:");
+            string n1 = Console.ReadLine();
+            Console.WriteLine("Inserisci il secondo numero:");
+            string n2 = Console.ReadLine();
+
+            // Parso i numeri
+            int iN1 = Convert.ToInt32(n1);
+            int iN2 = Convert.ToInt32(n2);
+
+            Console.WriteLine("Hai inserito: {0}, {1}", iN1, iN2);
+
+            // Faccio la moltiplicazione dei numeri 
+            Console.WriteLine("La loro moltiplicazione: {0}", Multi(iN1, iN2));
+        }
+
+        static int Multi(int n1, int n2)
+        { 
+            return n1 * n2;
+        }
+
+    }
+}
